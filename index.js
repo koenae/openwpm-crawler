bd1db81b72081ded1a5f1eeed87c92ef6e410a1b
Koen Aerts <koen.aerts@pm.me>
2021-02-15 21:41:37 +0100

    Inspect JavaScript content

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var level = require('level')
+const LineByLineReader = require('line-by-line')
+const fsp = require('fs').promises
 
 var db = level('./content')
+
+let dir = './hashcontent/'
 
 level('content.ldb', { createIfMissing: false }, function (err, db) {
   if (err instanceof level.errors.OpenError) {
     console.log('failed to open database')
   }
 
-  db.get('b0484bb78a832eefe3549afb313d52399e5b6de182d904da07d5b9da820848ac', function (err, val) {
-    console.log('value: ', val)
+  let lr = new LineByLineReader('hashes')
+  lr.on('line', (line) => {
+    db.get(line, function (err, content) {
+        if (content.includes('cmp(') || content.includes('tcfapi(')) {
+            console.log(line, ' contains tcf function')
+            storeContent(line, content)
+        }
+    })
   })
 })
+
+async function storeContent(fileName, content) {
+    await fsp.appendFile(dir + fileName, content)
+}
